d72554795b6808d24f17885cbb7a47ba06a36981
adam-pinto <adampinto@tamu.edu>
2023-09-06 00:05:57 +0000

    added test cases

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,9 +4,12 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[title]', with: 'book 1'
+    fill_in 'book[author]', with: 'author 1'
+    fill_in 'book[price]', with: '9.99'
+    fill_in 'book[published_date]', with: '09/05/2023'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('book 1')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'book 1', author: 'author 1', price: '9.99', published_date: '09/05/2023')
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +14,17 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is valid with author attribute' do
+    expect(subject.author).to be_present
+  end
+
+  it 'is valid with price attribute' do
+    expect(subject.price).to be_present
+  end
+
+  it 'is valid with published_date attribute' do
+    expect(subject.published_date).to be_present
+  end
+
 end
